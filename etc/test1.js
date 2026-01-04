1d68aaf7dbd41e8fd2a394c4f9f7341b36b23d27
DarkLight0418 <samseong0418@naver.com>
2026-01-04 10:54:28 +0900

    20250104 test1

---
+++ etc/test1.js
@@ -0,0 +1,15 @@
+const wo = () => {
+  for (let i = 1; i <= 9; i++) {
+    console.log(`2 * ${i} = ${2 * i}`)
+  }
+}
+
+wo()
+
+const wo2 = function () {
+  for (let i = 1; i <= 9; i++) {
+    console.log(`2 * ${i} = ${2 * i}`)
+  }
+}
+
+wo2()
