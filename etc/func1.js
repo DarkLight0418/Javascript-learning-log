08959d5411113f219e8ddd6b4eba046a9dd11bf7
DarkLight0418 <samseong0418@naver.com>
2026-01-23 10:32:09 +0900

    20260123 익명 함수 사용법 알아두기

---
+++ etc/func1.js
@@ -0,0 +1,17 @@
+// 매개변수가 없는 익명 함수
+let hi = function () {
+  console.log("hi");
+}
+
+// 매개변수가 없는 화살표 함수
+let hi0 = () => {
+  console.log("hi")
+}
+
+// 매개변수가 있는 익명 함수
+let sum = function (x, y) {
+  return x + y
+}
+
+// 매개변수가 있는 화살표 함수
+let sum0 = (x, y) => x + y;
