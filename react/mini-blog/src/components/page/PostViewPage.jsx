cbf6b6dea7445fc3f330535013318b91e156afb9
DarkLight0418 <samseong0418@naver.com>
2026-03-25 18:25:22 +0900

    [feat] 2026-03-25: PostViewPage.jsx 완료

--- react/mini-blog/src/components/page/PostViewPage.jsx
+++ react/mini-blog/src/components/page/PostViewPage.jsx
@@ -45,3 +45,56 @@ const CommentLabel = styled.p`
   font-size: 16px;
   font-weight: 500;
 `
+
+/*
+  postViewPage 컴포넌트에서는 먼저 프롭스로 전달받은 글의 ID를 이용해서 전체 데이터에서 해당되는 글을 찾습니다. 
+  그리고 찾은 글의 제목,내용,댓글을 랜더링하게 되고 그 아래에는 TextInput 컴포넌트와 Button 컴포넌트를 이용해 댓글을 작성할 수 있도록 ui를 제공했습니다.
+*/
+
+function PostViewPage(props) {
+  const navigate = useNavigate()
+  const { postId } = useParams()
+
+
+  const post = data.find((item) => {
+    return item.id == postId
+  })
+
+  const [comment, setComment] = useState("")
+
+  return (
+    <Wrapper>
+      <Container>
+        <Button
+          title="뒤로 가기"
+          onClick={() => {
+            navigate("/")
+          }}
+        />
+        <PostContainer>
+          <TitleText>{post.title}</TitleText>
+          <ContentText>{post.content}</ContentText>
+        </PostContainer>
+
+        <CommentLabel>댓글</CommentLabel>
+        <CommentList comments={post.comments} />
+
+        <TextInput
+          height={40}
+          value={comment}
+          onChange={(event) => {
+            setComment(event.target.value)
+          }}
+        />
+        <Button
+          title="댓글 작성하기"
+          onClick={() => {
+            navigate("/")
+          }}
+        />
+      </Container>
+    </Wrapper>
+  )
+}
+
+export default PostViewPage
